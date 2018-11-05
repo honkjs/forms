521358be0a3117b63fa2bc94dbdcf254ea352607
Kellen Piffner <k@8labs.com>
2018-11-05 11:32:13 -0800

    Splitting the input binders apart from the form.

--- README.md
+++ README.md
@@ -25,7 +25,7 @@
 
 A helper for keeping track of a forms model state changes.
 
-An experiment originally designed for [choo](https://github.com/choojs/choo).
+An experiment originally designed for use with [choo](https://github.com/choojs/choo).
 
 ```
 npm install @honkjs/forms
@@ -34,33 +34,68 @@ npm install @honkjs/forms
 # Basics
 
 ```ts
-// the context is where you store meta information about your model like validation errors.
-// we're also going to add a reference to the actual model so it's available later.
-const context = { errors: [], model: myModel };
+import { createForm, bindInput } from '@honks/forms';
 
-function onFormChange(ctx) {
+// the model is any basic object
+const myModel = {
+  name: 'Kel',
+  items: [{ id: 0, description: 'cool item' }, { id: 1, description: 'another cool item' }],
+};
+
+// the context is where you store meta information about your model like validation errors.
+const context = { errors: [] };
+
+function onFormChange(form) {
   // do things when the form changes
-  // note: only the context is available
 }
 
 // the form helper takes a model, context, and an onchange event handler
 const form = createForm(myModel, context, onFormChange);
 
-form.set((m) => (m.Name = 'fargles'));
-const fname = form.helper.input('Name', validations))
-
-// and here's an example of a form being used to bind to a model
+// All the real magic is in the bind helpers
+// bindInput attaches the onchange event to the form based on the field name
+// with typescript, the 'name' is only valid if the field exists on the model
 export function editorForm(form) {
   return html`
     <form>
       <input
         type="text"
         value=${form.model.Name || ''}
-        onchange=${form.input('Name')}
+        onchange=${bindInput(form, 'name')}
         placeholder="Name" required=true />
     </form>`;
 }
+```
 
-// create a form with the same context and change event as the parent form
-const itemForm = form.branch(myModel.items[0]);
+# Validation
+
+The bind helpers take an optional array of 'validators'.
+
+```ts
+function myValidator(value, fieldName, form) {
+  if (!value) {
+    // there's no requirement in how these work
+    // track errors and form info anyway you need to
+    form.context.errors.push('Name is required!');
+
+    // by default, the model value will be changed
+    // after all validators are ran.
+    // throw an error if that's undesired.
+  }
+}
+
+bindInput(form, 'name', [myValidator]
+```
+
+# Converters
+
+Similar to validators, you can create a custom input converter. (By default it makes a best 'guest' based on the input field type)
+
+```ts
+function myConverter(event) {
+  // do any magic necessary to pull your value from the input event
+  return event.target.value;
+}
+
+bindInput(form, 'name', [], myConverter);
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /**
- * A form helper context.  Access to input helpers, context, and the model.
+ * A form helper context.  Access to the context data, model, setter.
  */
-export interface IFormContext<TContext, TModel> extends IFormHelper<TContext, TModel> {
+export interface IFormContext<TContext, TModel> {
   /**
    * Reference to the model representing this form
    */
@@ -9,85 +9,13 @@ export interface IFormContext<TContext, TModel> extends IFormHelper<TContext, TM
 
   /**
    * General context passed to validators, onchange events, etc.
-   * Can contain the model, errors, whatever necessary.
+   * Can contain errors or whatever is necessary.
    */
   context: TContext;
 
   set: (mutate: (model: TModel) => void) => void;
 
-  /**
-   * Creates a form with the same context and onchange handler, but different model.
-   * Useful to work with collections or sub-models.
-   * Equivalent to:  createForm(newModel, oldForm.context, oldFormOnChange)
-   */
-  branch: <TBranchModel>(model: TBranchModel) => IFormContext<TContext, TBranchModel>;
-}
-
-/**
- * Form event binding helpers.
- *
- * @export
- * @interface IFormHelper
- * @template TContext
- * @template TModel
- */
-export interface IFormHelper<TContext, TModel> {
-  /**
-   * Creates an input onchange binding for a model field.
-   *
-   * @template K
-   * @param {K} field The field to bind to.
-   * @param {((value: TModel[K], context: TContext) => void)[]} [validators] An optional array of validation functions.
-   * @param {(e: Event) => TModel[K]} [converter] An optional convertor to parse the Event into the model value.  By default will use the 'type' field to parse correctly.
-   * @returns {(e: Event) => void} Returns a function that can be bound to an input field.
-   */
-  input: <K extends keyof TModel>(
-    field: K,
-    validators?: ((value: TModel[K], context: TContext) => void)[],
-    converter?: (e: Event) => TModel[K]
-  ) => (e: Event) => void;
-}
-
-function validate<TContext, TTarget, K extends keyof TTarget>(
-  context: TContext,
-  target: TTarget,
-  field: K,
-  value: TTarget[K],
-  validators?: ((value: TTarget[K], context: TContext) => void)[]
-) {
-  if (validators) {
-    for (let i = 0; i < validators.length; i++) {
-      validators[i](value, context);
-    }
-  }
-  target[field] = value;
-}
-
-/**
- * Converts an input event value into the appropriate type
- * based on the target "type" field.
- */
-function inputConverter(e: any) {
-  if (!e.target.type || e.target.type === 'text') {
-    return e.target.value;
-  }
-
-  if (e.target.type === 'number') {
-    return parseInt(e.target.value);
-  }
-}
-
-function createFormHelper<TContext, TModel>(
-  model: TModel,
-  context: TContext,
-  onchange: (context: TContext) => void
-): IFormHelper<TContext, TModel> {
-  return {
-    input: (field, validations, converter = inputConverter) => (e) => {
-      validate(context, model, field, converter(e), validations);
-      onchange(context);
-    },
-  };
+  onchange: (form: IFormContext<TContext, TModel>) => void;
 }
 
 /**
@@ -100,18 +28,59 @@ function createFormHelper<TContext, TModel>(
 export function createForm<TModel, TContext>(
   model: TModel,
   context: TContext,
-  onchange: (context: TContext) => void
+  onchange: (form: IFormContext<TContext, TModel>) => void
 ): IFormContext<TContext, TModel> {
-  const helper = createFormHelper<TContext, TModel>(model, context, onchange);
-
-  return {
+  const form: IFormContext<TContext, TModel> = {
     model,
     context,
+    onchange,
     set: (mutate) => {
       mutate(model);
-      onchange(context);
+      onchange(form);
     },
-    branch: (m) => createForm(m, context, onchange),
-    ...helper,
   };
+
+  return form;
+}
+
+export interface IFormFieldValidator<TContext, TModel, K extends keyof TModel> {
+  (value: TModel[K], field: K, form: IFormContext<TContext, TModel>): void;
+}
+
+export function bindInput<TContext, TModel, K extends keyof TModel>(
+  form: IFormContext<TContext, TModel>,
+  field: K,
+  validators?: IFormFieldValidator<TContext, TModel, K>[],
+  converter: (e: Event) => TModel[K] = inputConverter
+) {
+  return (e: Event) => {
+    const val = converter(e);
+    validate(form, field, val, validators);
+    form.set((m) => (m[field] = val));
+  };
+}
+
+function validate<TContext, TModel, K extends keyof TModel>(
+  form: IFormContext<TContext, TModel>,
+  field: K,
+  value: TModel[K],
+  validators?: IFormFieldValidator<TContext, TModel, K>[]
+) {
+  if (validators) {
+    for (let i = 0; i < validators.length; i++) {
+      validators[i](value, field, form);
+    }
+  }
+}
+
+/**
+ * Converts an input event value into the appropriate type
+ * based on the target "type" field.
+ */
+function inputConverter(e: any) {
+  if (e.target.type === 'number') {
+    return parseInt(e.target.value);
+  } else {
+    return e.target.value;
+  }
 }
